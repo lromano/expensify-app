51ed173325eeeae5289710964dc370b59654fc66
Lucas Romano <romano.lucas@gmail.com>
2019-03-18 19:45:52 +0100

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -17,4 +18,9 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
-ReactDOM.render(jsx, document.getElementById('app'));
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
